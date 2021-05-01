bf921c641186cd5c9592f367ff45cddc58dffaf1
manqobasukati <manqoba.sukati@peoplesized.com>
2021-05-01 15:22:03 +0200

    commit change

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   devServer: {
     https: true,
   },
-  pwa: { name: 'TsengisaPOS', themeColor: '#E1D15B' },
+  pwa: { name: 'TsengisaPOS', themeColor: '#E1D15B',display:'standalone' },
 };
