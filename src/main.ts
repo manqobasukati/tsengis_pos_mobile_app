a41bb6233af930381b2f872a341c4dac61a3f311
manqobasukati <manqoba.sukati@peoplesized.com>
2021-05-01 14:32:14 +0200

    commit this

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import './registerServiceWorker'
+
 Vue.config.productionTip = false;
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,5 @@ module.exports = {
   devServer: {
     https: true
   },
-  pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dist/service-worker.js',
-      // ...other Workbox options...
-    }
-  }
+  
 };
